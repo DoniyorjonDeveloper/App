9d99e9bc1bc4521fe82344c15efbe90881a39027
DoniyorjonDeveloper <developerdoni@gmail.com>
2020-07-26 02:09:32 +0500

    Commit

---
+++ NewProject1/app/src/main/java/intellect/dev/newproject1/App.kt
@@ -0,0 +1,14 @@
+package intellect.dev.newproject1
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object{
+        lateinit var instance : App
+    }
+}
